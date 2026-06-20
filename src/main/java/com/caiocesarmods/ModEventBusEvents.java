ff68148e1989d2df36ff6a83458c876d45d8b5bd
Caio202Cesar <caiodnobre@gmail.com>
2026-06-20 18:01:51 -0300

    .

---
+++ src/main/java/com/caiocesarmods/ModEventBusEvents.java
@@ -0,0 +1,23 @@
+package com.caiocesarmods;
+
+import com.caiocesarmods.caiolabradorsmod.CaioLabradorsMod;
+import com.caiocesarmods.caiolabradorsmod.entity.ModEntityTypes;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = CaioLabradorsMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvents {
+    @SubscribeEvent
+    public static void addEntityAttributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntityTypes.LABRADOR_ENTITY.get(), TocoToucanEntity.setCustomAttributes().create());
+
+    }
+
+    @SubscribeEvent
+    public static void onRegisterEntities(RegistryEvent.Register<EntityType<?>> event) {
+        ModSpawnEggItem.initSpawnEggs();
+    }
+}
