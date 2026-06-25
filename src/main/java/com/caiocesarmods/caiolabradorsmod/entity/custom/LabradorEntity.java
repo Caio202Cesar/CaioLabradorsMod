e9f8799b90e9761ad076575a49eb93796d868b9d
Caio202Cesar <caiodnobre@gmail.com>
2026-06-25 16:03:16 -0300

    .

--- src/main/java/com/caiocesarmods/caiolabradorsmod/entity/custom/LabradorEntity.java
+++ src/main/java/com/caiocesarmods/caiolabradorsmod/entity/custom/LabradorEntity.java
@@ -153,7 +153,7 @@ public class LabradorEntity extends WolfEntity {
         this.goalSelector.addGoal(3, new AvoidEntityGoal<>(this, LlamaEntity.class, 24.0F, 1.5D, 1.5D));
         this.goalSelector.addGoal(4, new LeapAtTargetGoal(this, 0.4F));
         this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
-        this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, true));
         //this.goalSelector.addGoal(7, new FetchItemGoal(this));
         this.goalSelector.addGoal(7, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(8, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
